d6769436aabee9f1a98c0f722ec0a368c078c572
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-25 12:33:12 +0100

    Update load-project.js

--- src/packager/load-project.js
+++ src/packager/load-project.js
@@ -37,7 +37,7 @@ const fromURL = async (url, progressCallback) => {
 
 const fromID = (id, token, progressCallback) => {
   const tokenPart = token ? `?token=${token}` : '';
-  const url = `https://editor-compiler.onrender.com/${id}${tokenPart}`;
+  const url = `https://editor-compiler.onrender.com/json/${id}${tokenPart}`;
   return fromURL(url, progressCallback);
 };
 
